b5e2c644af3f6350ca23d950be9372627dd0e3b4
ZinaQatr <>
2021-04-14 19:58:51 +0000

    done

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('welcome in my home page');
+
+var old = prompt ('how old are your baby ?');
+if(old>18){
+  alert('This page is dedicated to children');
+}else if(old<18){
+  alert('you are welcome');
+}
+
+var gender = prompt ('what is your baby gander ? boy or girl');
+alert ('do you like toys?');
+var toys  = prompt ('what is your baby toys ?');
+var clothes  = prompt ('what is your baby type clothes ?');
+alert('can you evaluate my home page ?');
+var evaluate = prompt('what is your evaluate ?')
+console.log(old);
+console.log(gender);
+console.log(toys);
+console.log(clothes);
+document.write(evaluate);
+document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQunl-jHUZlhAEitsz15FrLoDqejUS2QBvjBA&usqp=CAU">');
